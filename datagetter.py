aed705363bd66e3bde0fc3a5a06553168eb82cf3
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-06-28 13:03:39 -0500

    better variable names

--- datagetter.py
+++ datagetter.py
@@ -2,21 +2,28 @@ import requests
 import json
 
 class Retriever(object):
-	def __init__(self, heroname='', playerid=0):
+	def __init__(self, entry='', playerid=0):
 		with open('heros.json', 'r') as myfile:
 			data = myfile.read()
-			self.hero = heroname
+			self.hero = entry
 			self.id = playerid
-			self.heroIDs = json.loads(data)
+			self.heroID = 0
+			self.heroValues = json.loads(data)
 			self.api = 'https://api.opendota.com/api/heroes/{}/matches'
+
+	def recents(self):
+		api = 'https://api.opendota.com/api/heroes/{}/matches'
 
 	def call(self):
 		self.realName = ''
-		for i in self.heroIDs:
+		for i in self.heroValues:  # verify the hero name exists
 			if len(self.hero) > 1 and self.hero in i['name']:
-				self.realName = i['localized_name']
-				print(self.realName)
+				self.hero = i['localized_name']
+				self.heroID = i['id']
+				print('The hero {} exists! It has an ID Number of {}'.format(self.hero, self.heroID))
 				break
+
+
 
 t = Retriever('void')
 t.call()
