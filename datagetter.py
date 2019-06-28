43c0a0862fa84310353dc882ad44c61069be1ca3
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-06-28 12:11:21 -0500

    checks if the hero exists in the heros json

--- datagetter.py
+++ datagetter.py
@@ -1,15 +1,22 @@
 import requests
 import json
 
-
 class Retriever(object):
-    def __init__(self, heroname='', playerid = 0):
-        with open('heros.json', 'r') as myfile:
-            data = myfile.read()
+	def __init__(self, heroname='', playerid=0):
+		with open('heros.json', 'r') as myfile:
+			data = myfile.read()
+			self.hero = heroname
+			self.id = playerid
+			self.heroIDs = json.loads(data)
+			self.api = 'https://api.opendota.com/api/heroes/{}/matches'
 
-        self.hero = heroname
-        self.id = playerid
-        self.heroIDs = json.loads(data)
+	def call(self):
+		self.realName = ''
+		for i in self.heroIDs:
+			if len(self.hero) > 1 and self.hero in i['name']:
+				self.realName = i['localized_name']
+				print(self.realName)
+				break
 
-    def call(self):
-        api = 'https://api.opendota.com/api/recentMatches='
+t = Retriever('void')
+t.call()
