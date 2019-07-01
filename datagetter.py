9bc59a8d180055cfcb70661ba59661657adbecee
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-07-01 13:00:18 -0500

    JSON data for all hero stats is now in its own file

--- datagetter.py
+++ datagetter.py
@@ -9,21 +9,18 @@ class Retriever(object):
 			self.id = playerid
 			self.heroID = 0
 			self.heroValues = json.loads(data)
-			self.api = 'https://api.opendota.com/api/heroes/{}/matches'
-
-	def recents(self):
-		api = 'https://api.opendota.com/api/heroes/{}/matches'
 
 	def call(self):
-		self.realName = ''
 		for i in self.heroValues:  # verify the hero name exists
 			if len(self.hero) > 1 and self.hero in i['name']:
 				self.hero = i['localized_name']
 				self.heroID = i['id']
 				print('The hero {} exists! It has an ID Number of {}'.format(self.hero, self.heroID))
 				break
-
-
+		with open('current_Stats.json', 'w') as outfile:
+			data = requests.get('https://api.opendota.com/api/herostats').content
+			loaded = json.loads(data)
+			json.dump(loaded, outfile)
 
 t = Retriever('void')
 t.call()
