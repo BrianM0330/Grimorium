d7426e773da87317b5aaef05e31c7bc4a1a31baf
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-07-01 12:36:58 -0500

    writing json data to a new file WIP

--- datagetter.py
+++ datagetter.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import time
 
 class Retriever(object):
 	def __init__(self, entry='', playerid=0):
@@ -10,27 +9,20 @@ class Retriever(object):
 			self.id = playerid
 			self.heroID = 0
 			self.heroValues = json.loads(data)
-			self.api = 'https://api.opendota.com/api/heroes/{}/matches'
 
 	def recents(self):
-		self.api = 'https://api.opendota.com/api/heroes/{}/matches'.format(self.heroID)
-		print(self.api)
-		request = requests.get(self.api)
-
+		self.api.format(self.heroID)
 
 	def call(self):
-		self.realName = ''
 		for i in self.heroValues:  # verify the hero name exists
 			if len(self.hero) > 1 and self.hero in i['name']:
 				self.hero = i['localized_name']
 				self.heroID = i['id']
 				print('The hero {} exists! It has an ID Number of {}'.format(self.hero, self.heroID))
 				break
-		print('Please wait while I find match data on this hero...')
-		time.sleep(2)
-		self.recents()
-
-
+		with open('current_Stats.json', 'w') as outfile:
+			data = requests.get('https://api.opendota.com/api/herostats').content
+			json.dump(data, outfile)
 
 t = Retriever('void')
 t.call()
