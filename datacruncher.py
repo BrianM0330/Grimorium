1ec85b71460c28e7f5284c18138d9b20c003bc3b
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-07-02 15:22:05 -0500

    better imports

--- datacruncher.py
+++ datacruncher.py
@@ -1,11 +1,14 @@
-import plotly
-import pandas
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import json
 
 def analyze():
 	with open('current_Stats.json') as infile:
 		data = json.load(infile)
 	for i in data:
-		print( i['legs'] )
+		name = i['localized_name']
+		legCount = i['legs']
+		winRate1 = i['1_win'] / i['1_pick']
+		print( name + '\t' +  str(legCount)+ '\t'  + str(winRate1) )
 
 analyze()
