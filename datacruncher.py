7dfc3474b5c7b2e10531ab40ab2c7b5cd1ea7535
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-07-02 12:57:45 -0500

    open json file to prepare for reading the data

--- datacruncher.py
+++ datacruncher.py
@@ -2,3 +2,10 @@ import plotly
 import pandas
 import json
 
+def analyze():
+	with open('current_Stats.json') as infile:
+		data = json.load(infile)
+	for i in data:
+		print( i['legs'] )
+
+analyze()
