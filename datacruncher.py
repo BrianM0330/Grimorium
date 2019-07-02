45e1052f8a5424788b72e47d0acae855248e8436
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-07-02 15:53:23 -0500

    turned into a subclass of dataGetter

--- datacruncher.py
+++ datacruncher.py
@@ -1,14 +1,25 @@
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from datagetter import Retriever
 import json
 
-def analyze():
-	with open('current_Stats.json') as infile:
-		data = json.load(infile)
-	for i in data:
-		name = i['localized_name']
-		legCount = i['legs']
-		winRate1 = i['1_win'] / i['1_pick']
-		print( name + '\t' +  str(legCount)+ '\t'  + str(winRate1) )
 
-analyze()
+class Crunchy(Retriever):
+
+	def __init__(self):
+		super().__init__(self)
+		if self.hero.len() <= 0:
+			self.promptforHero()
+
+	def analyze(self):
+		with open('current_Stats.json') as infile:
+			data = json.load(infile)
+		for i in data:
+			name = i['localized_name']
+			legCount = i['legs']
+			winRate1 = i['1_win'] / i['1_pick']
+			print( name + '\t' +  str(legCount)+ '\t'  + str(winRate1) )
+
+	plot( [ {'x': [1,2,3], 'y': [3,1,6]    }   ]   )
+
+t = Crunchy('')
