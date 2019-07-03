8101d5ce4d7c9bcece633c0749c5974bfc387a19
BrianM0330 <36512389+BrianM0330@users.noreply.github.com>
2019-07-03 11:41:50 -0500

    added prompts for checking if a hero exists fixes for heroes with spaces

--- datagetter.py
+++ datagetter.py
@@ -3,14 +3,26 @@ import json
 
 class Retriever(object):
 	def __init__(self, entry='', playerid=0):
-		with open('heros.json', 'r') as myfile:
-			data = myfile.read()
-			self.hero = entry
+			self.heroDefaults()
+			self.hero = entry.lower()
+			if len(self.hero) == 0:
+				self.hero = self.promptforHero()
 			self.id = playerid
 			self.heroID = 0
-			self.heroValues = json.loads(data)
+
+	def heroDefaults(self):
+		with open('heros.json', 'r') as myfile:
+			data = myfile.read()
+		self.heroValues = json.loads(data)
+
+	def promptforHero(self):
+		self.hero = input('Hello! Could you please enter the hero you want stats for?')
+		return self.hero.lower()
 
 	def call(self):
+		if ' ' in self.hero:    #before calling for stats, make sure hero will be valid
+			self.hero = self.hero.replace(' ', '_')
+
 		for i in self.heroValues:  # verify the hero name exists
 			if len(self.hero) > 1 and self.hero in i['name']:
 				self.hero = i['localized_name']
